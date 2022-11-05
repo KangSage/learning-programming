bcceb39256ffba0182974a7ad8e6644097f8528f
KangSage <ksage@knou.ac.kr>
2022-11-05 17:40:51 +0900

    vite typescript 절대 경로 설정 완료

--- getting-start-react/vite.config.ts
+++ getting-start-react/vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from "vite-tsconfig-paths";
+import { fileURLToPath, URL } from 'url';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()]
-})
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  plugins: [react(), tsconfigPaths()],
+});
