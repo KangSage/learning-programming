438c184dc2c2f05f05806af3ec95641d7d550056
KangSage <ksage@knou.ac.kr>
2023-02-04 17:36:11 +0900

    JDBC 개발 - 조회

--- spring-db/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ spring-db/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 /**
  * JDBC - DriverManager 사용
@@ -27,6 +28,33 @@ public class MemberRepositoryV0 {
       throw e;
     } finally {
       close(conn, pstmt, null);
+    }
+  }
+
+  public Member findById(String memberId) throws SQLException {
+    String sql = "select * from member where member_id = ?";
+    Connection conn = null;
+    PreparedStatement pstmt = null;
+    ResultSet rs = null;
+    try {
+      conn = getConnection();
+      pstmt = conn.prepareStatement(sql);
+      pstmt.setString(1, memberId);
+      rs = pstmt.executeQuery();
+      // rs 객체 내부에 cursor가 존재하여 next를 한번 호출 해야한다.
+      if (rs.next()) {
+        Member member = new Member();
+        member.setMemberId(rs.getString("member_id"));
+        member.setMoney(rs.getInt("money"));
+        return member;
+      } else {
+        throw new NoSuchElementException("member not found memberId=" + memberId);
+      }
+    } catch (SQLException e) {
+      log.error("db error", e);
+      throw e;
+    } finally {
+      close(conn, pstmt, rs);
     }
   }
 
--- spring-db/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ spring-db/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -2,9 +2,12 @@ package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 class MemberRepositoryV0Test {
@@ -12,7 +15,13 @@ class MemberRepositoryV0Test {
   MemberRepositoryV0 repository = new MemberRepositoryV0();
   @Test
   void crud() throws SQLException {
-    Member member = new Member("memberV1", 10000);
+    Member member = new Member("memberV3", 10000);
     repository.save(member);
+
+    Member foundMember = repository.findById(member.getMemberId());
+    log.info("foundMember = {}", foundMember);
+    log.info("member == foundMember = {}", member == foundMember);
+    log.info("member equals foundMember = {}", member.equals(foundMember));
+    assertThat(foundMember).isEqualTo(member);
   }
 }
